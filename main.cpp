74a17226881cd47acaa04bacc69c6c1f57aba8d2
roissy <roissy@tuta.io>
2016-07-28 14:47:55 +0300

    c++

---
+++ main.cpp
@@ -0,0 +1,55 @@
+/**
+    l0l - Exploit Development Kit
+    Author:roissy
+    Greetz:b3mb4m
+
+    File : main.c
+*/
+
+#include "main.h"
+
+int main(){
+
+    banner();
+
+    char command[100];
+
+    // Ctrl+C Detect
+    signal(SIGINT, ctrlcDetect);
+
+    while(1){
+
+        printf("l0l >");
+        gets(command);
+        char *str;
+
+        str = strtok(command, " ");
+
+        // Commands..
+
+        if (strlen(command) == 0 || command[0] == '\r' || command[0] == '\n');
+
+        else if (strcmp(str,"help") == 0){
+            help();
+        }
+        else if (strcmp(str,"banner") == 0){
+            banner();
+        }
+        else if (strcmp(str,"clear") == 0){
+            clear();
+        }
+        else if(strcmp(str,"os") == 0){
+            char *os =  shorter(command,3);
+            printf("\n");
+            system(os);
+            printf("\n");
+        }
+        else if (strcmp(str,"exit") == 0){
+            closeApp();
+        }
+        else{
+            printf("\n[-] Unknown command: %s\n\n", str);
+        }
+}
+    return 0;
+}
