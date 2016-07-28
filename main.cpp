ca6972c2a32566807f4966ace1999dc5c4a60a0b
roissy <roissy@tuta.io>
2016-07-28 16:38:49 +0300

    update

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(){
 
     banner();
 
-    char command[100], command2[100];
+    char command[100];
 
     // Ctrl+C Detect
     signal(SIGINT, ctrlcDetect);
@@ -21,54 +21,40 @@ int main(){
 
         printf("l0l >");
         gets(command);
-        // scanf("%s", command);
-        int readKaka = 0, writeKaka;
-
-        while(command[readKaka] == ' '){
-            readKaka++;
-        }
-
-        for(writeKaka = 0;command[readKaka] != '\0'; readKaka++){
-          if(command[readKaka]==' ' && command[readKaka-1]==' '){
-              continue;
-          }
-          command2[writeKaka] = command[readKaka];
-          writeKaka++;
-        }
-        command2[writeKaka] = '\0';
 
         char *str;
-        str = strtok(command2, " ");
+        str = strtok(command, " ");
 
         // Commands..
 
         if (strlen(command) == 0 || command[0] == '\r' || command[0] == '\n');
 
-        else if (strcmp(str," ") == 0){
+        else if (strcmp(command,"help") == 0){
             help();
         }
 
-        else if (strcmp(str,"help") == 0){
-            help();
-        }
-        else if (strcmp(str,"banner") == 0){
+        else if (strcmp(command,"banner") == 0){
             banner();
         }
-        else if (strcmp(str,"clear") == 0){
+
+        else if (strcmp(command,"clear") == 0){
             clear();
         }
-        else if(strcmp(str,"os") == 0){
+
+        else if(strcmp(command,"os") == 0){
             char *os =  shorter(command,3);
             printf("\n");
             system(os);
             printf("\n");
         }
-        else if (strcmp(str,"exit") == 0){
+
+        else if (strcmp(command,"exit") == 0){
             closeApp();
         }
+
         else{
-            printf("\n[-] Unknown command: %s\n\n", str);
+            printf("\n[-] Unknown command: %s\n\n", command);
         }
-    }
+}
     return 0;
 }
