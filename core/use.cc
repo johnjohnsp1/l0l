9cb2f4506db53058c3d43267316f82eb5c851fe1
roissy <roissy@tuta.io>
2016-08-07 22:15:20 +0300

    use.cc

--- core/use.cc
+++ core/use.cc
@@ -1,86 +1,107 @@
-/**
-    l0l - Exploit Development Kit
-    Author:roissy
-    Greetz:B3mb4m
-
-    File : use.c
-
-
-        char command[100] = "", *str;
-
-        mainConsole:
-        // Set Blue Color
-        rlutil::setColor(9);
-        cout << "l0l >";
-        // Set White Color
-        rlutil::setColor(15);
-        cout << twopoint;
-        // Set Red Color
-        rlutil::setColor(12);
-        cout << usetxt;
-        rlutil::setColor(9);
-
-        printf(" >");
-
-        // Set White Color
-        rlutil::setColor(15);
-
-        gets(command);
-
-
-
-                else if(strcmp(command, "back") == 0){
-            twopoint="";
-            usetxt ="";
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-
-
-                string twopoint = "", usetxt = "";
+string usetxt, type;
 
                 str =  shorter(command,4);
 
                 if(command[4] == '\0'){
-                        rlutil::setColor(2);
-                         cout <<
-                                "\nuse Command\n"
-                                "=============\n"
-                                "\tDescription: Execute the specified module.\n"
-                                "\tUsage: use (Module)\n"
-                                "\tExp: use teat\n\n"
-                                "To see the modules, use the show command.\n\n";
+                    useCommand();
                 }
 
                 else{
-
-
-                        if(strcmp(str, "test") == 0){
-                            // Set Green Color
-                            rlutil::setColor(2);
-                            cout << "\nIs a Test Module (:\n\n";
-
-                            twopoint = ":";
+                        // Injector Test Module
+                        if(strcmp(str, "patpat") == 0){
                             usetxt=str;
+                            type = "injector";
                         }
+
+                        // Encoder Test Module
+                        else if(strcmp(str, "kaka") == 0){
+                            usetxt=str;
+                            type = "encoder";
+                        }
+
+                        // Shellcode Test Module
+                        else if(strcmp(str, "taktak") == 0){
+                            usetxt=str;
+                            type = "shellcode";
+                        }
+
+                        // Backdoor Test Module
+                        else if(strcmp(str, "hoppidi") == 0){
+                            usetxt=str;
+                            type = "backdoor";
+                        }
+
 
                         else{
                             // Set Red Color
                             rlutil::setColor(12);
                             printf("[-] Unknown module: %s\n", str);
+                            goto mainConsole;
                         }
 
-                }
+                        do{
 
+                            char usecmd[100] = "", *str2;
+
+                            rlutil::setColor(9);
+                            cout << "l0l";
+                            // Set White Color
+                            rlutil::setColor(15);
+                            cout << ":";
+                            // Set Red Color
+                            rlutil::setColor(12);
+                            cout << usetxt;
+                            rlutil::setColor(9);
+                            printf(" >");
+                            // Set White Color
+                            rlutil::setColor(15);
+
+                            gets(usecmd);
+
+                            strtok(usecmd, " ");
+
+                            if (strlen(command) == 0 || command[0] == '\r' || command[0] == '\n');
+
+                            // Use Commands
+
+                            if(strcmp(usecmd, "back") == 0){
+                                goto mainConsole;
+                            }
+                            else if (strcmp(usecmd,"exit") == 0){
+                                closeApp();
+                            }
+                            else if(strcmp(usecmd,"os") == 0){
+
+                                // Set White Color
+                                rlutil::setColor(15);
+
+                                str2 =  shorter(usecmd,3);
+                                printf("\n");
+
+                                if(usecmd[3] == '\0'){
+                                    osCommand();
+                                }else{
+                                    system(str2);
+                                }
+                                printf("\n");
+                            }
+                            else if(strcmp(usecmd, "clear") == 0){
+                                clear();
+                            }
+                            else if(strcmp(usecmd, "help") == 0){
+                                    if(type == "injector"){
+                                        injectorHelp();
+                                    }
+                                    else if(type == "encoder"){
+                                        encoderHelp();
+                                    }
+                                    else if(type == "shellcode"){
+                                        shellcodeHelp();
+                                    }
+                                    else if(type == "backdoor"){
+                                        backdoorHelp();
+                                    }
+                            }
+
+                        }while(1);
+                }
